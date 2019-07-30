cf21f6227ec9331bdb7c4c2c6fed99f81e481bb9
yangzhey <yang.zheyu@mail.utoronto.ca>
2019-07-30 11:52:10 +0800

    20190730 11:52 frontend

--- pages/ask_risk/ask_risk.js
+++ pages/ask_risk/ask_risk.js
@@ -1,7 +1,6 @@
 // pages/ask_risk/ask_risl.js
 Page({
   skipToRiskChoices: function(options) {
-    console.log('lalala')
     wx.navigateTo({
       url: '../index/index'
     })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,39 @@
 //index.js
 //获取应用实例
-const app = getApp()
+const app = getApp();
+var str1 = "谨慎型"
+var str2 = "稳健型"
+var str3 = "平衡型"
+var str4 = "进取型"
+var str5 = "激进型"
 
 Page({
   data: {
-    slider1: 1,
+    str: str1
+
   },
   changeSlider1(e) {
-    this.setData({slider1: e.detail.value})
-  }
+    if (e.detail.value == 1) {
+      this.setData({ str: str1 })
+    }
+    else if (e.detail.value == 2) {
+      this.setData({ str: str2 })
+    }
+    else if (e.detail.value == 3) {
+      this.setData({ str: str3 })
+    }
+    else if (e.detail.value == 4) {
+      this.setData({ str: str4 })
+    }
+    else if (e.detail.value == 5) {
+      this.setData({ str: str5 })
+    }
+  },
+
+  jumpToRecommendation: function (options) {
+    wx.navigateTo({
+      url: '../recommendation/recommendation'
+    })
+  },
 })
+
