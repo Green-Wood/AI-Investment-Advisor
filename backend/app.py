c01ca2d7e980fe5147eba5256ed101647b7c92d0
赵文祺 <1027572886a@gmail.com>
2019-07-31 09:36:54 +0800

    redirect http add cors

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask
 from flask_restplus import Api
 from werkzeug.middleware.proxy_fix import ProxyFix
 from config import config
@@ -13,6 +13,15 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config.from_object(config[APP_ENV])
 
 api = Api(app, version='1.0', title='Investment Advisor Api', prefix='/api')
+
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 
 api.add_namespace(ns_allocation)
 api.add_namespace(ns_fund)
