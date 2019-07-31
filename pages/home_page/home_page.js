8bba4261fda686d5e679fb47a8680a4b24ffe6f3
chelseabai <841381252@qq.com>
2019-07-31 19:49:30 +0800

    functional app

--- pages/home_page/home_page.js
+++ pages/home_page/home_page.js
@@ -1,7 +1,16 @@
 Page({
-  jumpToWelcome: function (options) {
+  logo1: function (options) {
     wx.navigateTo({
       url: '../ask_risk/ask_risk'
     })
   },
+
+  data:{
+    // 组件所需的参数
+    nvabarData: {
+      showCapsule: 1, //是否显示左上角图标   1表示显示    0表示不显示
+      title: '原地起飞', //导航栏 中间的标题
+    }
+  }
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -9,8 +9,12 @@ const app = getApp();
 
 Page({
   data: {
-    str: 1
-
+    str: 1,
+// 组件所需的参数
+    nvabarData: {
+      showCapsule: 1, //是否显示左上角图标   1表示显示    0表示不显示
+      title: '原地起飞', //导航栏 中间的标题
+    }
   },
   changeSlider1(e) {
     this.setData({ str: e.detail.value })
@@ -36,4 +40,7 @@ Page({
       url: '../recommendation/recommendation'
     })
   },
+
+  
+
 })
