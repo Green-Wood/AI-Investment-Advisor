eacc64b6501d19e98ab7d390351049ab551dcf95
XDTD <1355582364@qq.com>
2019-08-08 17:15:40 +0800

    第一版层次分析热力图

--- dash/plots/plot_heatmap.py
+++ dash/plots/plot_heatmap.py
@@ -11,11 +11,11 @@ import matplotlib.pyplot as plt
 PATH = pathlib.Path(__file__).parent.parent
 DATA_PATH = PATH.joinpath("data").resolve()
 path_adjusted_net_value = DATA_PATH.joinpath('adjusted_net_value.csv')
+path_instrument = '../data/instruments.csv'
 font_size = 8
 # 步长和窗口大小
 window = 30
 step = 3
-# todo: path_adjusted_net_value  adjusted_net_value.csv文件
 # todo: 字体大小 fig.layout.annotations[i].font.size
 
 
@@ -120,14 +120,122 @@ def plot_time_corr(codes: str):
     }
 
 
+def plot_heatmap_dendrogram(code_list: list):
+    """
+    :param (list) code_list: 基金代码列表
+    :return: (dict) fig{
+                    'data':
+                    'layout':
+            }
+    """
+    org_data = pd.read_csv(path_adjusted_net_value)
+    org_data = org_data[code_list]
+    fund_data = pd.read_csv(path_instrument)[['code','issuer']]
+    strs = [code + '' for code in code_list]
+    z = org_data.dropna().as_matrix()
+    codes = org_data.columns.values
+    data_array = z.T
+    labels = codes
+
+    # Initialize figure by creating upper dendrogram
+    fig = ff.create_dendrogram(data_array,orientation='bottom',labels=labels)
+    for i in range(len(fig['data'])):
+        fig['data'][i]['yaxis'] = 'y2'
+
+    # Create Side Dendrogram
+    dendro_side = ff.create_dendrogram(data_array,orientation='right')
+    for i in range(len(dendro_side['data'])):
+        dendro_side['data'][i]['xaxis'] = 'x2'
+
+    # Add Side Dendrogram Data to Figure
+    for data in dendro_side['data']:
+        fig.add_trace(data)
+
+    # Create Heatmap
+    dendro_leaves = dendro_side['layout']['yaxis']['ticktext']
+    dendro_leaves = list(map(int,dendro_leaves))
+    data_dist = pdist(data_array)
+    heat_data = squareform(data_dist)
+    heat_data = heat_data[dendro_leaves,:]
+    heat_data = heat_data[:,dendro_leaves]
+    heat_data_2 = np.round(heat_data,decimals=2)
+
+    texts = np.zeros(shape=(len(code_list),len(code_list)),dtype=list)
+    for i in range(len(texts)):
+        for j in range(len(texts[i])):
+            tmp1 = fund_data[fund_data['code'].isin([code_list[i]])]['issuer'].values[0]
+            tmp2 = fund_data[fund_data['code'].isin([code_list[j]])]['issuer'].values[0]
+            texts[i, j] ='基金公司1:' + tmp1 + '\r\n' + '基金公司2:' + tmp2 + '\r\n' + '样本距离' + str(heat_data_2[i,j])
+    heatmap = ff.create_annotated_heatmap(heat_data,
+                                          annotation_text=heat_data_2,
+                                          colorscale='blues',
+                                          showscale=True,
+                                          hoverinfo='text',
+                                          text=texts,
+                                          x=strs,
+                                          y=strs
+    )
+    heatmap['data'][0]['x'] = fig['layout']['xaxis']['tickvals']
+    heatmap['data'][0]['y'] = dendro_side['layout']['yaxis']['tickvals']
+
+    # Add Heatmap Data to Figure
+    for data in heatmap['data']:
+        fig.add_trace(data)
+
+    # Edit Layout
+    fig.update_layout({'width': 800,'height': 800,
+                       'showlegend': False,'hovermode': 'closest',
+                       })
+    # Edit xaxis
+    fig.update_layout(xaxis={'domain': [.15,1],
+                             'mirror': False,
+                             'showgrid': False,
+                             'showline': False,
+                             'zeroline': False,
+                             'ticks': ""})
+    # Edit xaxis2
+    fig.update_layout(xaxis2={'domain': [0,.15],
+                              'mirror': False,
+                              'showgrid': False,
+                              'showline': False,
+                              'zeroline': False,
+                              'showticklabels': False,
+                              'ticks': ""})
+
+    # Edit yaxis
+    fig.update_layout(yaxis={'domain': [0,.85],
+                             'mirror': False,
+                             'showgrid': False,
+                             'showline': False,
+                             'zeroline': False,
+                             'showticklabels': False,
+                             'ticks': ""
+                             })
+    # Edit yaxis2
+    fig.update_layout(yaxis2={'domain': [.825,.975],
+                              'mirror': False,
+                              'showgrid': False,
+                              'showline': False,
+                              'zeroline': False,
+                              'showticklabels': False,
+                              'ticks': ""})
+
+    # Plot!
+    # fig.show()
+    return {
+        'data':fig.data,
+        'layout':fig.layout
+    }
+
+
 if __name__ == '__main__':
     # test demo
     z = np.random.randn(20, 20)
     codes = ['257050', '000395','000001', '519050']
-    fig = plot_heatmap(codes)
+    fig = plot_heatmap_dendrogram(codes)
     print(fig)
-    codes = '257050 000395'
-    plot_time_corr(codes)
+    # codes = '257050 000395'
+    # plot_time_corr(codes)
 
     # org_data = pd.read_csv('../data/adjusted_net_value.csv')
     # org_data = org_data[['519661','257050','510150','377150','510650','510050','519050','270050','000150','519150','310368','686868','519668','000068','100068','519068','470068','000368','000057','080003','180003','550003','450003','213003','110003','630003','000003','620003','610103','161603','519003','090003','540003','630103','700003']]
