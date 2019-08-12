805eb1133170be32d068a79de5596f9d3a1dc593
赵文祺 <1027572886a@gmail.com>
2019-08-12 10:36:50 +0800

    添加热力图按风险值分配的接口

--- backend/resources/heatmap.py
+++ backend/resources/heatmap.py
@@ -3,6 +3,7 @@ from model.corr_map import get_corr
 import json
 from werkzeug.exceptions import NotFound
 from resources.efficient_frontier import _fund_list_parser
+from init_optmizer import optimizer
 
 api = Namespace('heatmap', description='根据基金代码列表，得到相关系数矩阵')
 
@@ -22,4 +23,24 @@ class HeatMap(Resource):
             json_str = get_corr(fund_list).to_json(orient='index')
         except KeyError:
             raise NotFound('Fund not find')
+        return json.loads(json_str)
+
+
+_risk_parser = reqparse.RequestParser()
+_risk_parser.add_argument('risk_index', type=float, help='能够接受的风险', choices=(0.01, 0.02, 0.03, 0.04, 0.05),
+                          required=True)
+
+
+@api.route('/risk')
+class BasedOnRisk(Resource):
+    @api.expect(_risk_parser)
+    def get(self):
+        """
+        根据风险值，得到相关系数矩阵，10只weight最高的基金
+        :return:
+        """
+        args = _risk_parser.parse_args()
+        ret, vol, sr, w = optimizer.get_fixed_ans(fixed='volatility', value=args['risk_index'])
+        fund_list = [x[0] for x in sorted(w.items(), key=lambda x: x[1], reverse=True)[:10]]
+        json_str = get_corr(fund_list).to_json(orient='index')
         return json.loads(json_str)
