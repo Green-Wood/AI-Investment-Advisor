f905acf032e9fa23db5516300b53a9bae95f7f72
XDTD <1355582364@qq.com>
2019-08-12 11:08:05 +0800

    将索引改为基金名称

--- backend/model/corr_map.py
+++ backend/model/corr_map.py
@@ -6,6 +6,7 @@ import pathlib
 PATH = pathlib.Path(__file__).parent.parent
 DATA_PATH = PATH.joinpath("data").resolve()
 path_adjusted_net_value = DATA_PATH.joinpath('adjusted_net_value.csv')
+path_instruments = DATA_PATH.joinpath('instruments.csv')
 
 
 def get_corr(code_list: list = None, adjusted_df: pd.DataFrame = None):
@@ -18,14 +19,26 @@ def get_corr(code_list: list = None, adjusted_df: pd.DataFrame = None):
             corr = get_corr(codes)
             print(corr)
     """
+    instruments_df = pd.read_csv(path_instruments)
+
     if code_list is None:
         pass
     if adjusted_df is None:
         adjusted_df = pd.read_csv(path_adjusted_net_value)[code_list]
+    # 建立索引
+    new_index = []
+    for code in adjusted_df.columns.values:
+        new_index.append(instruments_df['symbol'][instruments_df['code'].isin([code])].values[0])
     cov = risk_models.CovarianceShrinkage(adjusted_df).ledoit_wolf()
     var = np.eye(cov.shape[0]) * cov
     std = np.power(var, 0.5)
     I = np.linalg.inv(std)
     corr = I.dot(cov).dot(I)
-    corr_df = pd.DataFrame(corr, columns=adjusted_df.columns.values, index=adjusted_df.columns.values)
+    corr_df = pd.DataFrame(corr, columns=new_index, index=new_index)
     return corr_df
+
+
+if __name__ == '__main__':
+    codes = ['257050','000395','000001','519050']
+    corr = get_corr(codes)
+    print(corr)
